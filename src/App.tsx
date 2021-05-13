e751ab1667c6bc8761c29853fb5064fb2ee3cec8
jacekskowalski <jsk_profil@hotmail.com>
2021-05-13 23:14:14 +0200

    update mobile menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Header from "./header";
 import Main from "./main";
 import './sass/main.scss';
-
+import imageMobile from "./images/image-hero-mobile.jpg";
+import imagePC from "./images/image-hero-desktop.jpg";
 const App: React.FC = () => {
+  const [height, setHeight] = useState(0);
+  const ref =  useRef<HTMLImageElement | null>(null);
+const topMargin = 88;
+  useEffect(() => {
+    const nr: any= ref?.current?.clientHeight;
+    setHeight(nr -topMargin);
+  }, [height])
   return (
     <>
     <div className="container">
-      <Header />
+    <picture >
+       <source srcSet={imagePC} media="(min-width: 720px)" />
+       <source srcSet={imageMobile}  />
+                <img className="background__image" ref={ref} src={imageMobile} alt="An office desc" />
+           </picture>
+      <Header menuHeight={height}/>
       <Main />
       </div>
     </>
--- src/header.tsx
+++ src/header.tsx
@@ -2,13 +2,18 @@
 import React from 'react';
 import './sass/main.scss';
 
- const Header : React.FC = () => {
+type Props = {
+    menuHeight: number
+ };
+
+ const Header : React.FC<Props> = ({menuHeight}) => {
   const [menuState, setMenuState ]= React.useState(false);
-   const switchMenu =() => setMenuState(o => !o);
- 
+  const switchMenu =() => setMenuState(o => !o);
+  const nevbarLinkHeight= (menuHeight)/3;
     return (
         <>
     <div className="Navbar">
+       
         <div className="Navbar__Link logo">
            crowdfund
         </div>
@@ -16,19 +21,35 @@ import './sass/main.scss';
            { menuState ? <span >&#x2715;</span> : <span >&#9776;</span>
     }
         </div>
-        <div className={menuState ? "activeMobile" : "activePC"}>
+        { menuState ?
+        <div className="activeMobile" style={{height: menuHeight}}>
+        <nav className="Navbar__Items Navbar__Items--right">
+            <div className="Navbar__Link" style={{height: nevbarLinkHeight}}>
+                <a href="#" className="nav-links">About</a>
+            </div>
+            <div className="Navbar__Link" style={{height: nevbarLinkHeight}}>
+                <a href="#" className="nav-links">Discover</a>
+            </div>
+            <div className="Navbar__Link" style={{height: nevbarLinkHeight}}>
+                <a href="#" className="nav-links">Get Started</a>
+            </div>
+             </nav>           
+    </div> 
+    :
+    <div className="activePC">
         <nav className="Navbar__Items Navbar__Items--right">
             <div className="Navbar__Link">
                 <a href="#" className="nav-links">About</a>
             </div>
             <div className="Navbar__Link">
-                <a href="#" className="nav-links">PEOPLE</a>
+                <a href="#" className="nav-links">Discover</a>
             </div>
             <div className="Navbar__Link">
-                <a href="#" className="nav-links">WORK</a>
+                <a href="#" className="nav-links">Get Started</a>
             </div>
-             </nav>
+             </nav>           
     </div>
+ }
     </div>
         </>
     )
